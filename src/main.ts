a0f0930e2df57d49b170efc31c18401310b0e257
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2023-11-30 10:40:22 +0000

    added 1 card

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import "./styles/style.scss";
+
+import {pokemonArray} from "./data/pokemon";
+
+console.log(pokemonArray)
+
+  const card = document.querySelector<HTMLElement>(".card-container");
+
+  if (!card) {
+      throw new Error("Issue with card");
+    }
+
+  const showCards = (pokemon: Pokemon) => {
+    return  `<div class="card"> <div class="card__header-section">
+      <img class="card__image" src=${pokemon.sprite} /> </div>
+    
+    <div class="card__content">
+      <h2 class="card__heading">${pokemon.name}</h2>
+        <p class="card__text">${pokemon.name} #${pokemon.id} is a ${pokemon.types.join(" & ")} type pokemon.</p>
+      </div>`
+  }
+
+  card.innerHTML = showCards(pokemonArray[0]);
+ 
